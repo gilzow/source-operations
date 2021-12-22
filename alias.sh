c3341b69f3a445d8686be47bb7cba537e88fad02
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-22 14:20:21 -0600

    why is trying to find the full path to where you are located so difficult in bash???

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# where are we ?
-dirName=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# where are we, the long form ?
+dirName="$( cd -- "$( dirname -- "$0" )" > /dev/null && pwd -P)"
 # This assumes that main.py is in the same directory as this alias script
 function sourceOp() {
   python "${dirName}/main.py" "$@"
