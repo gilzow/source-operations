bae9099152ece21058cfdbcadfe322c3eb6a0052
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-22 14:34:16 -0600

    TIL dash doesnt support `function` declaration

--- alias.sh
+++ alias.sh
@@ -2,6 +2,6 @@
 # where are we, the long form ?
 dirName="$( cd -- "$( dirname -- "$0" )" > /dev/null && pwd -P)"
 # This assumes that main.py is in the same directory as this alias script
-function sourceOp() {
+sourceOp() {
   python "${dirName}/main.py" "$@"
 }
