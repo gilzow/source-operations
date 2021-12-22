c13c7fd856af083d555f5bfc158d16dc1844290a
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-22 14:35:36 -0600

    I am not a fan of dash

--- alias.sh
+++ alias.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # where are we, the long form ?
 dirName="$( cd -- "$( dirname -- "$0" )" > /dev/null && pwd -P)"
+echo "dirName is ${dirName}"
 # This assumes that main.py is in the same directory as this alias script
 sourceOp() {
   python "${dirName}/main.py" "$@"
