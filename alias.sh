afc7acd658336649dcdb93faa263684ee9f8a880
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-22 14:39:06 -0600

    seriously, why is this so difficult??!?

--- alias.sh
+++ alias.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # where are we, the long form ?
-dirName="$( cd -- "$( dirname -- "$0" )" > /dev/null && pwd -P)"
+dirName=="${BASH_SOURCE%/*}"
+if [[ ! -d "${dirName}" ]]; then dirName="$PWD"; fi
 echo "dirName is ${dirName}"
 # This assumes that main.py is in the same directory as this alias script
 sourceOp() {
