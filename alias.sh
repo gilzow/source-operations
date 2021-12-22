aea75b9f374b6b0fd755beb6373c4dee29a18a33
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-22 14:39:55 -0600

    fat-fingered the equal sign

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # where are we, the long form ?
-dirName=="${BASH_SOURCE%/*}"
+dirName="${BASH_SOURCE%/*}"
 if [[ ! -d "${dirName}" ]]; then dirName="$PWD"; fi
 echo "dirName is ${dirName}"
 # This assumes that main.py is in the same directory as this alias script
