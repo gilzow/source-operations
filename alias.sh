c75ef45d359ac35309caadc13401c606ef66d357
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-23 08:39:18 -0600

    removes functionality from alias.sh into the install.sh script renames main.py to sourceOp

--- alias.sh
+++ alias.sh
@@ -2,8 +2,8 @@
 # where are we, the long form ?
 # @todo I give up trying to figure out how to get dash to give me the full path to where
 # this file is located. I'll hard-code it for now and come back to it later
-dirName="${PLATFORM_SOURCE_DIR}/source-operations"
-# This assumes that main.py is in the same directory as this alias script
-sourceOp() {
-  python "${dirName}/main.py" "$@"
-}
+#dirName="${PLATFORM_SOURCE_DIR}/source-operations"
+## This assumes that main.py is in the same directory as this alias script
+#sourceOp() {
+#  python "${dirName}/main.py" "$@"
+#}
--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/gilzow/source-operations.git
-printf "Current value of PATH: %s" "${PATH}"
+IFS=':' read -ra PATHS <<< "${PATH}"
+for dir in "${PATHS[@]}"; do
+  printf "Evaluating %s\n" "${dir}"
+  sourceFile="sourceOp"
+  source="${PLATFORM_SOURCE_DIR}/source-operations/${sourceFile}"
+  if [ -d "${dir}" ] && [ -w "${dir}" ] && [ ! -e "${dir}/${sourceFile}" ]  && [ ! -L "${dir}/${sourceFile}" ]; then
+    printf "Creating link at %s for source %s" "${dir}/${sourceFile}" "${source}"
+    ln -s -f "$source" "${dir}/${sourceFile}"
+    break;
+  fi
+done
+
