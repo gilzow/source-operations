1dbc947d30f2a82801f2bd719ba6a53312faa5a3
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-29 14:13:41 -0600

    switches to us using a git clone instead of curl'ing every file we need

--- setup.sh
+++ setup.sh
@@ -2,56 +2,34 @@
 # bash <(curl -fsS https://raw.githubusercontent.com/gilzow/source-operations/main/test.sh) foobar
 # https://github.com/gilzow/source-operations.git
 
-dirSourceOps="/tmp/source-operations"
+gitSourceOps="https://github.com/gilzow/source-operations.git"
+tmpDir="/tmp"
+dirSourceOps="${tmpDir}/source-operations"
 
 #check and see if we already have the repo cloned in /tmp
-git -c "${dirSourceOps}" status
+git -C "${dirSourceOps}" status
 gitCheck=$?
 
-if (( 0 == gitCheck )); then
-  
+if (( 0 != gitCheck )) || [[ ! -d "${dirSourceOps}" ]]; then
+  git -C "${tmpDir}" "${gitSourceOps}"
+else
+  #let's make sure we're up-to-date
+  git -C "${dirSourceOps}" pull origin
 fi
 
-
-# Ensures we have our tmp directory set up
-# @return string name of the temp directory
-function makeTmpDir() {
-  name="/tmp/sourceops"
-  if [[ ! -d "${name}" ]]; then
-    mkdir -p "${name}"
-  fi
-
-  echo "${name}"
-}
-
-# Removes the "old" version of our scripts
-# @param string path *and* file to remove
-# @return bool exit status
-function removeOldVerion() {
-  file="${1}"
-  if [[ -f "${file}" ]]; then
-    rm -f "${file}"
-  fi
-}
-
-tmpDir=$(makeTmpDir) || exit 1
 
 perform=${1:-'nothing'}
 case ${perform} in
 
   autoprsourceop)
     printf "Beginning automated pull request after auto-update source operation..."
-    # grab our sourceop-support file, save it, source it, then fire off the main component
-    sourceopSupport="${tmpDir}/sourceops-support.sh"
-    # get rid of the old version in case we already have it
-    removeOldVerion "${sourceopSupport}"
-    curl -o "${sourceopSupport}" -fsSO https://raw.githubusercontent.com/gilzow/source-operations/auto-pr/sourceop-support.sh
-    . "${sourceopSupport}"
+    # grab our sourceop-support file, source it, then fire off the main component
+    . "${dirSourceOps}/sourceops-support.sh"
+    # run the set up and source operation
     (trigger_source_op) || exit 1
-    createGHPR="${tmpDir}/create-ghpr.sh"
-    removeOldVerion "${createGHPR}"
-    curl -o "${createGHPR}" -fsSO https://raw.githubusercontent.com/gilzow/source-operations/auto-pr/create-ghpr.sh
 
+    #now we're ready to start the PR process
+    . "{$dirSourceOps}/ghPR.sh"
 
     ;;
 
