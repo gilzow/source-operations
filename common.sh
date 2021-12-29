d944f6e49dab021e3eb05b968c57be1792e866ad
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-29 15:27:16 -0600

    cleans up spacing and extra period when warning about the psh cli token adds an extra new line before the event message

--- common.sh
+++ common.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 # Outputs the event and error message that was encountered during the process
+# Do NOT include a final period (.) at the end of your message. I'll do that for ya.
 # @todo For now this just echos an error message, but might expand to include slack notices?
 # @return void
 function logFatalError() {
   event="${1:-no event specificied}"
   message="${2:-no message provided}"
-  printf "Fatal error encountered during %s\n%s\n" "${event}" "${message}. Exiting."
+  printf "\nFatal error encountered during %s\n%s\n" "${event}" "${message}. Exiting.\n"
 }
 
 # ensures we have the token to use the psh cli tool
 # @return bool
 function checkForPSHToken() {
   if [[ -z ${PLATFORMSH_CLI_TOKEN+x} ]]; then
-    message="You will need to create an environmental variable 'PLATFORMSH_CLI_TOKEN' that contains a valid platform.sh "
-    message="${message} token before I can run platform.sh cli commands."
+    message="You will need to create an environmental variable 'PLATFORMSH_CLI_TOKEN' that contains a valid platform.sh"
+    message="${message} token before I can run platform.sh cli commands"
     logFatalError "PSH Token Check" "${message}"
     exit 1
   fi
